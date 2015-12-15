68819e82df8a84ac68cd3021a5285c40ab28fa6f
Alex LaFroscia <alex@lafroscia.com>
2015-12-15 12:20:46 -0500

    Add blueprint for adding dependencies

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var path = require('path');
 module.exports = {
   name: 'ember-cli-foundation-6-sass',
   included: function included(app) {
-    this.app = app;
+    this._super.included(app);
+
     var foundationPath = path.join(app.bowerDirectory, 'foundation-sites', 'scss');
     app.options.sassOptions = app.options.sassOptions || {};
     app.options.sassOptions.includePaths = app.options.sassOptions.includePaths || [];
