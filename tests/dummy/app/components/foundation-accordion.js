3d67ed86dc0e0788c34bc6f13b71c57365c305e2
mitchell stanley <ubuntumitch@gmail.com>
2016-01-08 18:11:04 +0000

    Update version number

---
+++ tests/dummy/app/components/foundation-accordion.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({  
+  didInsertElement: function() {
+    // Ember.$(document).foundation(); //or Ember.$(document).foundation();
+  }
+});
