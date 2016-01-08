a11957e740c43d3fccd21c361c53bbdf258e127a
Mac Stevens <macstevens@liquidhive.com>
2016-01-07 22:10:08 -0600

    Build logic to bring in javascript files Code that brings in _settings.scss file Helpers for CSS optimization Updated foundation to 6.1

--- index.js
+++ index.js
@@ -11,5 +11,21 @@ module.exports = {
     app.options.sassOptions = app.options.sassOptions || {};
     app.options.sassOptions.includePaths = app.options.sassOptions.includePaths || [];
     app.options.sassOptions.includePaths.push(foundationPath);
+
+    // Include the js paths
+    if (app.options.foundationJs) {
+      if ((typeof app.options.foundationJs == 'string') ||
+          (app.options.foundationJs instanceof String)) {
+        if (app.options.foundationJs === 'all') {
+          app.import(path.join(app.bowerDirectory, 'foundation-sites', 'dist', 'foundation.js'));
+        }
+      }
+      else if (options.foundationJs instanceof Array) {
+        options.foundationJs.forEach(function(componentName) {
+          var foundationJsPath = path.join(app.bowerDirectory, 'foundation-sites', 'js');
+          app.import(path.join(foundationJsPath, 'foundation.' + componentName + '.js'));
+        });
+      }
+    }
   }
 };
