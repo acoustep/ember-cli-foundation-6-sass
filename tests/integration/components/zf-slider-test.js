4686fc19a10999b0f5e0cc046e575bb7af821058
Garrick <gcheung@fitbit.com>
2016-06-19 21:55:23 -0700

    Removing toggle for demoing the reveal example. Adjust the slider test to be async, otherwise foundation will throw errors because there are async event handlers triggered in their code.

--- tests/integration/components/zf-slider-test.js
+++ tests/integration/components/zf-slider-test.js
@@ -12,8 +12,10 @@ test('it renders', function(assert) {
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
   // Template block usage:" + EOL +
+  let done = assert.async();
+
   this.render(hbs`
-    {{#zf-slider}}
+    {{#zf-slider }}
       <span class="slider-handle"  data-slider-handle role="slider" tabindex="1"></span>
       <span class="slider-fill" data-slider-fill></span>
       <input type="hidden">
@@ -21,5 +23,9 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$('.slider-handle').attr('role'), 'slider');
+
+  // changed.zf.slider is always executed, even if the slider hasn't actually moved.
+  // Use it to async complete this test.
+  this.$('.slider').on('changed.zf.slider', done);
 
 });
